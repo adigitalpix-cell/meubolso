e8984430ec8619604026f2f76a36f975a1ecb24a
adigitalpix-cell <adigitalpix@gmail.com>
2026-06-18 08:51:47 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.APP_BUILD_CONFIG = {
-  version: "1.0.0.23"
+  version: "1.0.0.24"
 };
 
 window.SUPABASE_CONFIG = {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "minhas-financas-v1.0.0.23";
+const CACHE_NAME = "minhas-financas-v1.0.0.24";
 const ASSETS = [
   "/",
   "/index.html",
