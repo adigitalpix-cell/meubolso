b45bb57cda50c82eee2844ab630d92a1f09b12e9
adigitalpix-cell <adigitalpix@gmail.com>
2026-07-13 08:38:37 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.APP_BUILD_CONFIG = {
-  version: "0.55.1"
+  version: "0.55.2"
 };
 
 window.SUPABASE_CONFIG = {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meu-bolso-v0.55.1";
+const CACHE_NAME = "meu-bolso-v0.55.2";
 const ASSETS = [
   "/",
   "/index.html",
